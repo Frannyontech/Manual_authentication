42f3160e5f016a1e068ff54e0c49fc5bd75e711e
Frannyontech <fran.galazojeda@gmail.com>
2021-10-14 12:23:45 -0300

    first commit destroy_session

--- Gemfile
+++ Gemfile
@@ -52,3 +52,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# 13/10/2021
+gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :stories
+  resources :users, only: [:new, :create, :show]
   root 'stories#index'
+  post 'users/destroy_session', as:'destroy_session'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
