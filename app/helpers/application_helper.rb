6b2db3c95156fb9fc327723129b2f80f5863fb30
Frannyontech <fran.galazojeda@gmail.com>
2021-10-18 12:15:29 -0300

    fix stories and users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+    def logged?
+        #true si session iniciada
+        #false usuario sin session iniciada
+        if current_user.present?
+            return true
+        else
+            return false
+        end
+    end
+
+    def current_user
+        if session[:user_id].present?
+            @user = User.find(session[:user_id])
+            return @user
+        else
+            return nil
+        end
+    end
 end
